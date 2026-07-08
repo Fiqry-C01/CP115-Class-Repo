cc06438d7072cbd336a95e9a51248c3e930af2a8
FIQRY CO1E <fzthetrue@gmail.com>
2026-07-08 12:33:29 +0800

    COMMIT EDDEN

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = int(input())
+megabyte = kilobyte * 0.001
+print(megabyte)
+gigabyte = megabyte * 0.001
+print(gigabyte)
+terabyte = gigabyte * 0.001
+print(terabyte)
+petabyte = terabyte * 0.001
+print(petabyte)
